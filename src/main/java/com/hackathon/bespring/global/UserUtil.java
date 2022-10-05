df0217060621ea367e9a1e636784e460747b0465
정대현 <eogus_2513@dsm.hs.kr>
2022-10-05 17:37:56 +0900

    🔀 :: getCurrentUser() method 로직 수정

--- src/main/java/com/hackathon/bespring/global/UserUtil.java
+++ src/main/java/com/hackathon/bespring/global/UserUtil.java
@@ -1,7 +1,8 @@
 package com.hackathon.bespring.global;
 
 import com.hackathon.bespring.domain.user.domain.User;
-import com.hackathon.bespring.global.security.auth.AuthDetails;
+import com.hackathon.bespring.domain.user.domain.repository.UserRepository;
+import com.hackathon.bespring.global.security.exception.UserNotFound;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -10,8 +11,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class UserUtil {
 
+    private final UserRepository userRepository;
+
     public User getCurrentUser() {
-        AuthDetails user = (AuthDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return user.getUser();
+        String accountId = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return userRepository.findByAccountId(accountId)
+                .orElseThrow(() -> UserNotFound.EXCEPTION);
     }
 }
